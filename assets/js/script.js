46ceeeeba2f53ef202af1116c44eeece0650613f
amanda iaria <amanda.iaria@gmail.com>
2015-02-17 17:37:35 -0500

    Cookie time

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,18 @@ $(function(){
             a.slideUp();
             $("footer.on").hide();
              $("footer.off").show();
-
+            bakeCookies("bake");
         }
         else{
 
             a.slideDown();
              $("footer.off").hide();
              $("footer.on").show();
+             bakeCookies("eat");
         }
 
    })
-
+   bakeCookies();
 
 
 
@@ -33,5 +34,46 @@ $(function(){
 })
 
 function bakeCookies(yum){
-
+    if(yum == "bake"){
+        createCookie("yum", "on", 1);
+    }
+    else if(yum == "eat"){
+        eraseCookie("yum");
+    }
+    else{
+        var ck = readCookie("yum");
+        if(ck == "on"){
+            $(".alert article").hide();
+            $("footer.on").hide();
+            $("footer.off").show();
+       }
+    }
 }
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+
+
